9c41d6749e36aa5cfa95af9339d3451da1d47d1e
cyril.cros <cyril.cros@polytechnique.org>
2024-05-13 17:22:32 +0200

    Fixes the choice of category
    The category should be choose in the Dataloader initialization and not
    with __getitem__

--- dataset.py
+++ dataset.py
@@ -5,65 +5,74 @@ import torch
 from PIL import Image
 
 class EMDataset(Dataset):
-   """A PyTorch dataset to load EM and masks"""
-   def __init__(self,root_dir, seed: int, categories=["mito", "ld", "nucleus"], transform=None, img_transform=None) -> None:
-       super().__init__()
-       self.seed = seed
-       self.categories = categories
-       self.root_dir = root_dir
-       self.samples = os.listdir(self.root_dir)  # list the samples
-       self.transform = (
+    """ Dataloader class for Chad's electron microscopy data
+    Extends torch.utils.data.Dataset
+    """
+    def __init__(self,root_dir: str, seed: int, category:str, transform=None, img_transform=None) -> None:
+        """Initialization
+
+        Args:
+            root_dir (str): A train / validattion / test folder with images and masks subfolder.
+            seed (int): A random number generator seed for the dataloader.
+            category (str): An organelle to segment.
+            transform (_type_, optional): Transformation to apply to both images and masks. Defaults to None.
+            img_transform (_type_, optional): Transformation that applies to masks only. Defaults to None.
+
+        Raises:
+            ValueError: If category is not one 'mito', 'ld', 'nucleus'.
+        """
+        super().__init__()
+        self.seed = seed
+        # check the categories
+        allowed_categories = ['mito', 'ld', 'nucleus']
+        if category not in allowed_categories:
+            raise ValueError(f"You asked for an item of category {category} which 
+                            is not one of {'/'.join(allowed_categories)}")
+        self.categories = category
+        self.root_dir = root_dir
+        self.samples = os.listdir(self.root_dir)  # list the samples
+        self.transform = (
             transform  # transformations to apply to both inputs and targets
         )
-       self.img_transform = img_transform  # transformations to apply to raw image only
+        self.img_transform = img_transform  # transformations to apply to raw image only
        #  transformations to apply just to inputs
-       inp_transforms = transforms.Compose(
-           [
-               #transforms.Grayscale(),
-               #transforms.ToTensor(),
-               #transforms.Normalize([0.5], [0.5]),  # 0.5 = mean and 0.5 = variance
-           ]
-       )
-       self.loaded_imgs = [None] * len(self.samples)
-       self.loaded_masks = [None] * len(self.samples)
-       for sample_ind in range(len(self.samples)):
-           img_path = os.path.join(
+        self.loaded_imgs = [None] * len(self.samples)
+        self.loaded_masks = [None] * len(self.samples)
+        for sample_ind in range(len(self.samples)):
+            img_path = os.path.join(
                self.root_dir, self.samples[sample_ind], "image.tif"
-           )
-           image = Image(img_path)
-           image.load()
-           self.loaded_imgs[sample_ind] = inp_transforms(image)
-        
-           mask_path = os.path.join(
-               self.root_dir, self.samples[sample_ind], "mask.tif"
-           )
-           mask = Image(mask_path)
-           mask.load()
-           self.loaded_masks[sample_ind] = transforms.ToTensor()(mask)
+            )
+            image = Image(img_path)
+            image.load()
+            self.loaded_imgs[sample_ind] = inp_transforms(image)
+            mask_path = os.path.join(
+                self.root_dir, self.samples[sample_ind], "mask.tif"
+            )
+            mask = Image(mask_path)
+            mask.load()
+            self.loaded_masks[sample_ind] = transforms.ToTensor()(mask)
 
    # get the total number of samples
-   def __len__(self):
-       return len(self.samples)
+    def __len__(self):
+        return len(self.samples)
 
-   # fetch the training sample given its index
-   def __getitem__(self, idx, category: str):
-       # check the categories
-       if category not in self.categories:
-           raise R
-       # we'll be using Pillow library for reading files
-       # since many torchvision transforms operate on PIL images
-       image = self.loaded_imgs[idx]
-       mask = self.loaded_masks[idx]
-       if self.transform is not None:
-           # Note: using seeds to ensure the same random transform is applied to
-           # the image and mask
-           torch.manual_seed(self.seed)
-           image = self.transform(image)
-           torch.manual_seed(self.seed)
-           mask = self.transform(mask)
-       if self.img_transform is not None:
-           image = self.img_transform(image)
-       return image, mask
+    # fetch the training sample given its index
+    def __getitem__(self, idx):
+        # we'll be using Pillow library for reading files
+        # since many torchvision transforms operate on PIL images
+        image = self.loaded_imgs[idx]
+        mask = self.loaded_masks[idx]
+        if self.transform is not None:
+            # Note: using seeds to ensure the same random transform is applied to
+            # the image and mask
+            seed = torch.seed()
+            torch.manual_seed(seed)
+            image = self.transform(image)
+            torch.manual_seed(seed)
+            mask = self.transform(mask)
+        if self.img_transform is not None:
+            image = self.img_transform(image)
+        return image, mask
    
 if __name__ == '__main__':
     train_dataset = EMDataset(root_dir='train')
