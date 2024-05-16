9351e224e861d418ede1362d88a2a3e970053442
Chad Moore <chad.moore@sydney.edu.au>
2024-05-16 12:07:32 +0200

    fix bugs

--- predictonfull_lsd.py
+++ predictonfull_lsd.py
@@ -1,5 +1,4 @@
 import torch
-import tifffile
 import daisy
 from funlib.persistence import open_ds, prepare_ds, Array
 from funlib.geometry import Coordinate, Roi
@@ -7,32 +6,25 @@ import numpy as np
 from torchvision import transforms
 import zarr
 
-# image = tifffile.imread("testimage/HM25_HighRes_Aligned0050(1).tif")
-image = zarr.open("test_data.zarr")["raw"][0, :, :]
-image = (
-    transforms.Normalize([0.5], [0.5])(transforms.ToTensor()(image))
-    .unsqueeze(0)
-    .float()
-    .numpy()
-)
+import logging
 
-img_array = Array(
-    image, roi=daisy.Roi((0, 0), (8000, 8000)), voxel_size=Coordinate(1, 1)
-)
+
+raw_array = open_ds("test_data.zarr", "raw")
+
 
 affs_array = prepare_ds(
-    "full_pred_example_ld.zarr",
+    "full_pred_3d_ld.zarr",
     "affs",
-    total_roi=img_array.roi,
-    voxel_size=(1, 1),
+    total_roi=raw_array.roi,
+    voxel_size=raw_array.voxel_size,
     dtype=np.float32,
     num_channels=2,
 )
 lsd_array = prepare_ds(
-    "full_pred_example_ld.zarr",
+    "full_pred_3d_ld.zarr",
     "lsds",
-    total_roi=img_array.roi,
-    voxel_size=(1, 1),
+    total_roi=raw_array.roi,
+    voxel_size=raw_array.voxel_size,
     dtype=np.float32,
     num_channels=6,
 )
@@ -46,32 +38,57 @@ def process_block():
 
         with client.acquire_block() as block:
             with torch.no_grad():
-                image = torch.from_numpy(
-                    img_array.to_ndarray(block.read_roi, fill_value=0)
-                ).cuda()
-                prediction = unet(image)
+
+                # READ THE IMAGE DATA
+                image = raw_array.to_ndarray(block.read_roi, fill_value=0)[
+                    0
+                ]  # (8000, 8000)
+                image = transforms.Normalize([0.5], [0.5])(
+                    transforms.ToTensor()(image)
+                ).unsqueeze(0).cuda()
+
+                logging.error(f"input range: ({image.min()}, {image.max()})")
+
+                # PREDICT ON GPU
+                prediction = unet(image)  # (1, 8, 8000, 8000)
+
+                logging.error((prediction.min(), prediction.max()))
+
+                # WRITE THE PREDICTION TO ZARR
                 affs_pred, lsd_pred = prediction[:, :2, :, :], prediction[:, 2:, :, :]
 
                 affs_pred = Array(
-                    affs_pred[0].cpu().numpy(), roi=block.read_roi, voxel_size=(1, 1)
+                    affs_pred.cpu().numpy().transpose([1, 0, 2, 3]),
+                    roi=block.read_roi,
+                    voxel_size=raw_array.voxel_size,
                 ).to_ndarray(block.write_roi)
 
-                affs_array[block.write_roi] = affs_pred
+                affs = np.clip(affs_pred * 255, 0, 255).astype(np.uint8)
+                logging.error(
+                    (affs.min(), affs.max(), affs_pred.min(), affs_pred.max())
+                )
+
+                affs_array[block.write_roi] = affs
 
                 lsd_pred = Array(
-                    lsd_pred[0].cpu().numpy(), roi=block.read_roi, voxel_size=(1, 1)
+                    lsd_pred.cpu().numpy().transpose([1, 0, 2, 3]),
+                    roi=block.read_roi,
+                    voxel_size=raw_array.voxel_size,
                 ).to_ndarray(block.write_roi)
 
-                lsd_array[block.write_roi] = lsd_pred
+                lsd_array[block.write_roi] = np.clip(lsd_pred * 255, 0, 255).astype(
+                    np.uint8
+                )
 
 
-context = Coordinate(16, 16)
-read_roi = Roi((0, 0), (800, 800)).grow(context, context)
-write_roi = Roi((0, 0), (800, 800))
+voxel_size = raw_array.voxel_size
+context = Coordinate(0, 16, 16) * voxel_size
+read_roi = Roi((0, 0, 0), (1, 800, 800)).grow(context, context) * voxel_size
+write_roi = Roi((0, 0, 0), (1, 800, 800)) * voxel_size
 
 task = daisy.Task(
-    "full_pred_lsd",
-    total_roi=img_array.roi.grow(context, context),
+    "full_pred_lsd_ld",
+    total_roi=raw_array.roi.grow(context, context),
     read_roi=read_roi,
     write_roi=write_roi,
     process_function=process_block,
